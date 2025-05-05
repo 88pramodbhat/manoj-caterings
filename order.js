d0d58bf1973b9abcafa830c4dca90ea03b20c064
Pramod bhat <163877863+88pramodbhat@users.noreply.github.com>
2025-05-05 18:05:29 +0530

    Update order.js

--- order.js
+++ order.js
@@ -32,7 +32,7 @@ const products = [
   {
     name: "CARROT HALWA",
     price: "1 kg 500",
-    image: "caterings%20image/Screenshot%202025-05-05%20162742.pn"
+    image: "caterings%20image/Screenshot%202025-05-05%20162742.png"
   },
   {
     name: "BUNS",
