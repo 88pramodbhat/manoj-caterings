5a35b413b6eeb6a79f95828cf46ca1d1f91b3bd5
Pramod bhat <163877863+88pramodbhat@users.noreply.github.com>
2025-05-05 21:38:38 +0530

    Update order.js

--- order.js
+++ order.js
@@ -1,47 +1,47 @@
 const products = [
   {
     name: "Idli",
-    price: "10 per idli",
+    price: "10",
     image: "caterings%20image/Screenshot%202025-05-05%20162438.png"
   },
   {
     name: "Uppit Mix",
-    price: "30 per plate",
+    price: "30",
     image: "caterings%20image/Screenshot%202025-05-05%20162518.png"
   },
   {
     name: "Avalakki (Poha)",
-    price: "30 per plate",
+    price: "30",
     image: "caterings%20image/Screenshot%202025-05-05%20162544.png"
   },
   {
     name: "VADA",
-    price: "15 per vada",
+    price: "15",
     image: "caterings%20image/Screenshot%202025-05-05%20162458.png"
   },
   {
     name: "DOSA",
-    price: "40 per dosa",
+    price: "40",
     image: "caterings%20image/Screenshot%202025-05-05%20162630.png"
   },
   {
     name: "JAMUN",
-    price: "10 per jamun",
+    price: "10",
     image: "caterings%20image/Screenshot%202025-05-05%20162700.png"
   },
   {
     name: "CARROT HALWA",
-    price: "1 kg 500",
+    price: "500",
     image: "caterings%20image/Screenshot%202025-05-05%20162742.png"
   },
   {
     name: "BUNS",
-    price: "20 per bun",
+    price: "20",
     image: "caterings%20image/Screenshot%202025-05-05%20163045.png"
   },
   {
     name: "AVALAKKI BATH",
-    price: "30 per plate",
+    price: "30",
     image: "caterings%20image/Screenshot%202025-05-05%20162825.png"
   }
 ];
@@ -55,7 +55,7 @@ products.forEach(product => {
     <img src="${product.image}" alt="${product.name}">
     <div class="details">
       <h2>${product.name}</h2>
-      <p class="price">${product.price}</p>
+      <p class="price">₹${product.price}</p>
       <button class="button see-options">See options</button>
       <button class="button add-to-cart">Add to Cart</button>
     </div>
@@ -64,7 +64,7 @@ products.forEach(product => {
 
   const addToCartButton = productEl.querySelector(".add-to-cart");
   addToCartButton.addEventListener("click", () => {
-    console.log(`${product.name} added to cart.`);
-    // You can add further logic here, like pushing to a cart array
+    // Redirect to cart page with name and price as URL parameters
+    window.location.href = `cart.html?name=${encodeURIComponent(product.name)}&price=${encodeURIComponent(product.price)}`;
   });
 });
